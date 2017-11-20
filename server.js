051472b51e0b7f14a85b94f4abf1d423c4ed027d
Guba Dmitriy <gudmian@gmail.com>
2017-11-20 23:20:51 +0300

    Create simple server

---
+++ server.js
@@ -0,0 +1,22 @@
+let http = require("http");
+let path = require("path");
+let express = require("express");
+let logger = require("winston");
+let socketIo = require("socket.io");
+
+
+const app = express();
+const server = http.Server(app);
+const mainSocket = socketIo(server);
+
+
+app.use(express.static(path.join(__dirname, "/static")));
+
+app.get("/", (req, res)=>{
+    res.sendfile("index.html");
+});
+
+
+server.listen(8080, ()=>{
+    console.log("Server run on port:", 8080);
+});
