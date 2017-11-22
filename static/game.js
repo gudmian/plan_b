a2847759525eb04c5c46b9d3041f674a1f502443
bassind <bassindanil@hotmial.com>
2017-11-22 18:42:12 +0300

    Code formatting

--- static/game.js
+++ static/game.js
@@ -64,30 +64,25 @@ document.addEventListener('keyup', (event) => {
 });
 
 
-
-
 function initMouseEvents() {
     // Mouse handling code
     // When the mouse is pressed it rotates the players view
-    dynamcCanvas.addEventListener("mouseup", function(event)
-    {
+    dynamicCanvas.addEventListener("mouseup", function (event) {
         movement.mouse_down = false;
     }, false);
 
-    dynamicCanvas.addEventListener("mousedown", function(event)
-    {
+    dynamicCanvas.addEventListener("mousedown", function (event) {
         movement.mouse_down = true;
     }, false);
 
-    dynamicCanvas.addEventListener("mousemove", function(event)
-    {
-        if(event.offsetX) {
-            movement.mouse_X = event.offsetX * staticCanvas.width/currentHeight;
-            movement.mouse_Y = event.offsetY * staticCanvas.height/currentHeight;
+    dynamicCanvas.addEventListener("mousemove", function (event) {
+        if (event.offsetX) {
+            movement.mouse_X = event.offsetX * staticCanvas.width / currentHeight;
+            movement.mouse_Y = event.offsetY * staticCanvas.height / currentHeight;
         }
-        else if(event.layerX) {
-            movement.mouse_X = event.layerX;
-            movement.mouse_Y = event.layerY;
+        else if (event.layerX) {
+            movement.mouse_X = event.offsetX * staticCanvas.width / currentHeight;
+            movement.mouse_Y = event.offsetY * staticCanvas.height / currentHeight;
         }
 
     }, false);
@@ -97,35 +92,35 @@ initMouseEvents();
 
 setInterval(() => {
     socket.emit("change state", movement);
-}, 1000/60);
+}, 1000 / 60);
 
 socket.on("render", (state) => {
     console.log("Rendering...")
     dynamicContext.clearRect(0, 0, dynamicCanvas.width, dynamicCanvas.height)
-	var TO_RADIANS = Math.PI/180;
+    var TO_RADIANS = Math.PI / 180;
 
     function renderPlayers() {
         //dynamicContext.fillStyle = "red";
         let players = state.playersInf;
-		tex_player = new Image;
-		tex_player.src = "static/textures/players/apier.png";
-		tex_player.onload = function () {
+        tex_player = new Image;
+        tex_player.src = "static/textures/players/apier.png";
+        tex_player.onload = function () {
             for (let id in players) {
                 let player = players[id];
                 if (socket.id == id) {
-					dynamicContext.fillStyle = "#00F";
-					dynamicContext.font = "italic 10pt Arial";
-					dynamicContext.fillText(player.health, player.posX-15, player.posY-20);
+                    dynamicContext.fillStyle = "#00F";
+                    dynamicContext.font = "italic 10pt Arial";
+                    dynamicContext.fillText(player.health, player.posX - 15, player.posY - 20);
                 }
-				let dx = player.posX;
-				let dy = player.posY;
+                let dx = player.posX;
+                let dy = player.posY;
                 dynamicContext.save();
-				dynamicContext.translate(dx,dy);
+                dynamicContext.translate(dx, dy);
                 dynamicContext.rotate(2 * Math.PI + player.angle);
-				dynamicContext.translate(-dx,-dy);
-				dynamicContext.drawImage(tex_player, player.posX-15, player.posY-15, 30, 30);
+                dynamicContext.translate(-dx, -dy);
+                dynamicContext.drawImage(tex_player, player.posX - 15, player.posY - 15, 30, 30);
                 dynamicContext.restore();
-			}
+            }
         }
     }
 
@@ -137,7 +132,7 @@ socket.on("render", (state) => {
         dynamicContext.fillStyle = "blue";
         let bullets = state.bulletsInf;
         for (let id in bullets) {
-            for(let bullet of bullets[id]){
+            for (let bullet of bullets[id]) {
                 dynamicContext.beginPath();
                 dynamicContext.arc(bullet.posX, bullet.posY, bullet.radius, 0, 2 * Math.PI);
                 dynamicContext.fill();
@@ -161,11 +156,11 @@ socket.on("render static", (map) => {
                 staticContext.fillRect(cell.posX, cell.posY, cell.size, cell.size);
             }
             else {
-				ground = new Image();
+                ground = new Image();
                 ground.src = "static/textures/grass00.png";
                 ground.onload = function () {
-					staticContext.drawImage(ground, cell.posX, cell.posY, cell.size, cell.size)
-				}
+                    staticContext.drawImage(ground, cell.posX, cell.posY, cell.size, cell.size)
+                }
             }
         }
     }
